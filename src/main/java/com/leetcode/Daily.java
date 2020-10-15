4093e92a81443d0cc349bc458097d39d13fe322d
Z <alex@ZdeMacBook-Pro.local>
2020-10-15 21:58:49 +0800

    leetcode 每日一题 5538. 统计子树中城市之间最大距离

--- src/main/java/com/leetcode/Daily.java
+++ src/main/java/com/leetcode/Daily.java
@@ -1,5 +1,9 @@
 package com.leetcode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class Daily {
 
     public static void sortColors1(int[] nums) {
@@ -62,4 +66,47 @@ public class Daily {
     public static void main(String[] args) {
         Daily.sortColors(new int[]{2,0,2,1,1,0});
     }
+
+    public Node connect(Node root) {
+        Queue<Node> queue = new LinkedList<>();
+        if (root != null) {
+            queue.add(root);
+        }
+        int i = 0;
+        while (!queue.isEmpty()) {
+            int count = (int)Math.pow(2, i);
+            Node next = null;
+            while (count != 0) {
+                Node node = queue.poll();
+                node.next = next;
+                next = node;
+                if (node.right != null) {
+                    queue.add(node.right);
+                    queue.add(node.left);
+                }
+                count--;
+            }
+            i++;
+        }
+        return root;
+    }
 }
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
